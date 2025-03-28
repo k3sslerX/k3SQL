f1d8ea28e8d94f08b5fef97cdbe6eeceda389181
k3ssler <jkessler365@mail.ru>
2025-03-28 22:27:59 +0300

    added package format for client

--- clientExec.go
+++ clientExec.go
@@ -1,16 +1,55 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"k3SQLServer/k3SQLClient"
 	"os"
+	"strconv"
 )
 
 func main() {
 	host := "localhost"
-	port := "3003"
+	port := 3003
+	var err error
 	if len(os.Args) == 3 {
 		host = os.Args[1]
-		port = os.Args[2]
+		port, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
-	k3SQLClient.Connect(host, port)
+	connection := k3SQLClient.K3Server{
+		Host:     host,
+		Port:     port,
+		Database: "k3db",
+		User:     "k3user",
+		Password: "333",
+	}
+	conn, err := k3SQLClient.Connect(connection)
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Println("Connected")
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Enter SQL query (exit - for exit)")
+	for {
+		fmt.Print("> ")
+		if !scanner.Scan() {
+			break
+		}
+		query := scanner.Text()
+		if query == "exit" {
+			break
+		}
+		resp, err := conn.Query(query)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Print(resp)
+		}
+	}
 }
