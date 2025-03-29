b523ef42fa906e220e15e38e4cb4997f35475f7a
k3ssler <jkessler365@mail.ru>
2025-03-29 14:23:31 +0300

    added connection set checker

--- k3SQLClient/structure.go
+++ k3SQLClient/structure.go
@@ -2,9 +2,12 @@ package k3SQLClient
 
 import "net"
 
-const ConnectionError = "connection error"
-const SendingFail = "sending failure"
-const ReadingFail = "reading failure"
+const (
+	ConnectionError    = "connection error"
+	SendingFail        = "sending failure"
+	ReadingFail        = "reading failure"
+	ConnectionIsNotSet = "connection is not set"
+)
 
 type K3Server struct {
 	Host     string
